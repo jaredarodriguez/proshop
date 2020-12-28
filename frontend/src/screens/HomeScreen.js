04b0376298fdaccc1291294b809a682fb246744d
Jared <jaredarodriguez@gmail.com>
2020-12-28 09:17:15 -0500

    homescreen renders product card attributes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import products from '../products'
 
-const HomeScreen = (key) => {
+const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
